b17b2f489848d0720aa6889a93db762ebdfd3cfa
milind55555 <hanchatemilind291@gmail.com>
2026-02-27 16:31:03 +0530

    Manifest ,dockerfile ,jenkinsfile and helm files with directory are addedgit add .

---
+++ terraform/main.tf
@@ -0,0 +1,35 @@
+resource "aws_security_group" "devops_sg" {
+  name = "devops-sg"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 30007
+    to_port     = 30007
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "devops_server" {
+  ami           = "ami-019715e0d74f695be"
+  instance_type = var.instance_type
+  key_name      = var.key_name
+  security_groups = [aws_security_group.devops_sg.name]
+
+  tags = {
+    Name = "Java-DevOps-Project"
+  }
+}
